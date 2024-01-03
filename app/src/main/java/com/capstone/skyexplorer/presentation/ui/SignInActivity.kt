d24225049f709997bf12138de9ca02b9b89e0e28
wildanfatah22 <awildanfatahillah@gmail.com>
2024-01-03 22:19:04 +0700

    initial project

---
+++ app/src/main/java/com/capstone/skyexplorer/presentation/ui/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.skyexplorer.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.skyexplorer.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
