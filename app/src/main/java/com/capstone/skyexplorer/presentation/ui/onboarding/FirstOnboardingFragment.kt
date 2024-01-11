952bafeab8543d51efa07b55b251cc9aa8e0b0b4
KMalif <khafidulmualif2019@gmail.com>
2024-01-12 06:58:50 +0700

    [Kmalif][Add] add logic fuction to onboarding screen

--- app/src/main/java/com/capstone/skyexplorer/presentation/ui/onboarding/FirstOnboardingFragment.kt
+++ app/src/main/java/com/capstone/skyexplorer/presentation/ui/onboarding/FirstOnboardingFragment.kt
@@ -5,17 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import com.capstone.skyexplorer.R
+import com.capstone.skyexplorer.databinding.FragmentFirstOnboardingBinding
 
 
 class FirstOnboardingFragment : Fragment() {
-
+    private lateinit var binding: FragmentFirstOnboardingBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first_onboarding, container, false)
+        binding = FragmentFirstOnboardingBinding.inflate(inflater, container, false)
+        slideToNextFragment()
+        return binding.root
+    }
+    private fun slideToNextFragment(){
+        val onboardingViewpagerContainer = activity?.findViewById<ViewPager2>(R.id.onBoardingContainer)
+        binding.btnNext.setOnClickListener {
+            onboardingViewpagerContainer?.currentItem = 1
+        }
     }
 
 }
--- app/src/main/java/com/capstone/skyexplorer/presentation/ui/onboarding/SpalshScreenFragment.kt
+++ app/src/main/java/com/capstone/skyexplorer/presentation/ui/onboarding/SpalshScreenFragment.kt
@@ -1,6 +1,7 @@
 package com.capstone.skyexplorer.presentation.ui.onboarding
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,7 +14,21 @@ class SpalshScreenFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        moveToAnotherFragment()
         return inflater.inflate(R.layout.fragment_spalsh_screen, container, false)
+    }
+
+    private fun  moveToAnotherFragment(){
+        Handler().postDelayed({
+            if (checkOnboardingFinished()){
+                //
+            }else {
+
+            }
+        }, 3000)
+    }
+    private fun checkOnboardingFinished(): Boolean {
+        return false
     }
 
 }
