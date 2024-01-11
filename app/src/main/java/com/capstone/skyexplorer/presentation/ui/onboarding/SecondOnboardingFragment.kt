e36846b8e9bfc3bfc494178b06cfec95ebb7639a
KMalif <khafidulmualif2019@gmail.com>
2024-01-12 06:58:50 +0700

    [Kmalif][Add] add logic fuction to onboarding screen

--- app/src/main/java/com/capstone/skyexplorer/presentation/ui/onboarding/SecondOnboardingFragment.kt
+++ app/src/main/java/com/capstone/skyexplorer/presentation/ui/onboarding/SecondOnboardingFragment.kt
@@ -5,17 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import com.capstone.skyexplorer.R
+import com.capstone.skyexplorer.databinding.FragmentSecondOnboardingBinding
 
 
 class SecondOnboardingFragment : Fragment() {
-
+    private lateinit var binding: FragmentSecondOnboardingBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second_onboarding, container, false)
+        binding = FragmentSecondOnboardingBinding.inflate(inflater, container, false)
+        slideToNextFragment()
+        return binding.root
+    }
+
+    private fun slideToNextFragment(){
+        val onboardingViewpagerContainer = activity?.findViewById<ViewPager2>(R.id.onBoardingContainer)
+        binding.btnNext.setOnClickListener {
+            onboardingViewpagerContainer?.currentItem = 1
+        }
     }
 
 }
--- app/src/main/java/com/capstone/skyexplorer/presentation/ui/onboarding/ThirdOnboardingFragment.kt
+++ app/src/main/java/com/capstone/skyexplorer/presentation/ui/onboarding/ThirdOnboardingFragment.kt
@@ -6,15 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.capstone.skyexplorer.R
+import com.capstone.skyexplorer.databinding.FragmentThirdOnboardingBinding
 
 class ThirdOnboardingFragment : Fragment() {
-
+    private lateinit var binding: FragmentThirdOnboardingBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third_onboarding, container, false)
+        binding = FragmentThirdOnboardingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    private fun finishOnboarding(){
+
     }
 
 }
