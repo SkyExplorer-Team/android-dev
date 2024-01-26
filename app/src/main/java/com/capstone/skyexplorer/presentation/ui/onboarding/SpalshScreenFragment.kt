b19e088cd877857715f25883a6dc7b8c6097a34b
Rzldo <ezas4531@gmail.com>
2024-01-26 12:10:21 +0700

    set skipped onboarding data store

--- app/src/main/java/com/capstone/skyexplorer/presentation/ui/onboarding/SpalshScreenFragment.kt
+++ app/src/main/java/com/capstone/skyexplorer/presentation/ui/onboarding/SpalshScreenFragment.kt
@@ -6,29 +6,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.capstone.skyexplorer.R
-
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+@AndroidEntryPoint
 class SpalshScreenFragment : Fragment() {
+    private val viewModel : OnboardingViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        moveToAnotherFragment()
         return inflater.inflate(R.layout.fragment_spalsh_screen, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getShowOnBoarding()
+        moveToAnotherFragment()
+    }
     private fun  moveToAnotherFragment(){
         Handler().postDelayed({
-            if (checkOnboardingFinished()){
-                findNavController().navigate(R.id.action_spalshScreenFragment_to_mainOnboardingFragment)
-            }else {
-                findNavController().navigate(R.id.action_spalshScreenFragment_to_signinMethodFragment)
+            lifecycleScope.launch {
+                viewModel.state.collect{
+                    if (it.showOnBoarding){
+                        findNavController().navigate(R.id.action_spalshScreenFragment_to_mainOnboardingFragment)
+                    }else {
+                        findNavController().navigate(R.id.action_spalshScreenFragment_to_signinMethodFragment)
+                    }
+                }
             }
         }, 3000)
     }
-    private fun checkOnboardingFinished(): Boolean {
-        return true
-    }
+
 
 }
--- app/src/main/java/com/capstone/skyexplorer/presentation/ui/signin/SignInFragment.kt
+++ app/src/main/java/com/capstone/skyexplorer/presentation/ui/signin/SignInFragment.kt
@@ -5,15 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.capstone.skyexplorer.R
 import com.capstone.skyexplorer.databinding.FragmentSignInBinding
 import com.capstone.skyexplorer.databinding.FragmentSigninMethodBinding
-
-
+import com.capstone.skyexplorer.model.auth.login.LoginResponse
+import com.capstone.skyexplorer.model.auth.login.PostLoginModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+@AndroidEntryPoint
 class SignInFragment : Fragment() {
     private var _binding: FragmentSignInBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: LoginViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,9 +30,18 @@ class SignInFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launch {
+            viewModel.state.collect { uiState ->
+                loadingStateIsToggled(uiState.isLoading)
+                errorStateIsToggled(uiState.isError)
+                uiState.loginResult?.let { successStateIsToggled(it) }
+            }
+        }
         binding.apply {
             btnSignIn.setOnClickListener {
-
+                viewModel.login(
+                    PostLoginModel(email = "", password = "")
+                )
             }
             tvForgotPasswod.setOnClickListener {
                 findNavController().navigate(
@@ -34,5 +49,14 @@ class SignInFragment : Fragment() {
                 )
             }
         }
+    }
+    private fun loadingStateIsToggled(isLoading : Boolean){
+
+    }
+    private fun errorStateIsToggled(isError : Boolean){
+
+    }
+    private fun successStateIsToggled(loginResult : LoginResponse){
+
     }
 }
